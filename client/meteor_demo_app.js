86e57703aacc35f71333e03b04c621efcd548e7f
Sasi <sasidharan@thydreamtech.com>
2014-11-07 18:25:12 +0530

    fixed message box position render

--- client/meteor_demo_app.js
+++ client/meteor_demo_app.js
@@ -3,14 +3,13 @@ UserLog = new Meteor.Collection('login');
 PositionLog = new Meteor.Collection('position');
 
 if (Meteor.isClient) {
-  // counter starts at 0
 
   Router.map(function () {
-    this.route('conversation',{
+    this.route('conversation', {
     });
     this.route('newuser');
-    this.route('login',{
-      path:'/'
+    this.route('login', {
+      path: '/'
 
     });
   });
@@ -18,32 +17,27 @@ if (Meteor.isClient) {
   Template.conversation.helpers({
 
     username: function () {
-      if(Session.get('user') == null){
+      if (Session.get('user') == null) {
         Router.go('login')
-      }else {
+      } else {
         return Session.get('user')
       }
     },
-    message:function () {
-     var chats = Chat.find({});
-      chats.forEach(function(data){
-       var elm_position_datum = PositionLog.findOne({'id_no':data._id});
-        if(elm_position_datum) {
-          console.log($('.draggable').text());
-            $('#' + data._id).css('top', elm_position_datum.top);
-            $('#' + data._id).css('left', elm_position_datum.left);
-      };
+    message: function () {
+      var chats = Chat.find({});
+      chats.forEach(function (data) {
+        var elm_position_datum = PositionLog.findOne({'id_no': data._id});
+        if (elm_position_datum) {
+          $('#' + data._id).css('top', elm_position_datum.top);
+          $('#' + data._id).css('left', elm_position_datum.left);
+        }
+        ;
 
-    });
+      });
       return chats
     }
   });
 
-//  Template.conversation.rendered = function(){
-//     console.log("Test" + $(".draggable").size());
-//    $(".draggable").draggable();
-//  }
-//
   Template.newuser.events({
     'click #submit': function () {
       UserLog.insert({
@@ -58,48 +52,51 @@ if (Meteor.isClient) {
 
   Template.login.events({
     'click #sign-in': function () {
-     var user =$('.user');
-     var password =$('.password');
-     var userdata = UserLog.findOne({user:user.val(),password:password.val()});
-     if(!(userdata== null) && !(userdata._id == null)){
-       Session.set('user',user.val());
-       Router.go('conversation')
-     }else{
-       alert("Invalid username and password");
-     }
+      var user = $('.user');
+      var password = $('.password');
+      var userdata = UserLog.findOne({user: user.val(), password: password.val()});
+      if (!(userdata == null) && !(userdata._id == null)) {
+        Session.set('user', user.val());
+        Router.go('conversation')
+      } else {
+        alert("Invalid username and password");
+      }
     },
     'click #sign-up': function () {
-        Router.go('newuser')
+      Router.go('newuser')
     }
   });
 
   Template.conversation.events({
     'click .add': function () {
-      // increment the counter when button is clicked
-       Chat.insert({
-         user: Session.get('user'),
-         content: $('#message').val(),
-         top:null,
-         left:null
-       });
+      Chat.insert({
+        user: Session.get('user'),
+        content: $('#message').val(),
+        top: null,
+        left: null
+      });
       $('#message').val('');
     },
     'click .logout': function () {
       Router.go('login');
-      Session.set('user',null);
+      Session.set('user', null);
     },
     'click .delete': function () {
       Chat.remove(this._id)
     },
-    'mouseup .draggable.initialized': function () {
-      var pos=$('#'+this._id).position();
-      var datum = PositionLog.findOne({'id_no':this._id})
-      if(datum){
-        PositionLog.update(datum._id,{$set :{'top': pos.top, 'left': pos.left}});
-      }else {
+    'dragstop .draggable.initialized': function () {
+      var pos = {
+        top: $('#' + this._id).css('top'),
+        left: $('#' + this._id).css('left')
+      };
+      console.log(pos);
+      var datum = PositionLog.findOne({'id_no': this._id})
+      if (datum) {
+        PositionLog.update(datum._id, {$set: {'top': pos.top, 'left': pos.left}});
+      } else {
         PositionLog.insert({'id_no': this._id, 'top': pos.top, 'left': pos.left});
       }
     }
-   });
+  });
 }
 
